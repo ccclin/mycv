61420f7cfa541077aa7a582ab3f71c3c769092dd
C.lin <demeter.yeh@gmail.com>
2015-07-27 16:41:23 +0800

    add show.html.erb

--- app/controllers/myself_controller.rb
+++ app/controllers/myself_controller.rb
@@ -1,5 +1,12 @@
 class MyselfController < ApplicationController
   before_action :authenticate_user!
-  def index
+  def show
+    @user = User.find(user_id_getted)
+  end
+
+  private
+
+  def user_id_getted
+    params[:id] || 1
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'myself#index'
+  root 'myself#show'
   devise_for :users
   resources 'myself'
 end
